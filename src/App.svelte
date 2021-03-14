7442655f9097b4351dbe98aa8aeadcbf10aa6792
Kasper <kasperkh.kh@gmail.com>
2021-03-15 00:07:54 +0100

    Remove unnecessary stuff

--- src/App.svelte
+++ src/App.svelte
@@ -9,26 +9,3 @@
   })
 </script>
 
-<style lang="sass">
-  :global(body)
-    margin: 0
-    font-family: Arial, Helvetica, sans-serif
-    font-size: 18px
-  main
-    text-align: center
-    min-height: 100vh
-    display: flex
-    flex-direction: column
-    justify-content: center
-    code
-      background: #e6e6e6
-      padding: 3px 6px
-      border-radius: 4px
-</style>
-
-<template lang="pug">
-  main
-    h1 Hello world
-    p Edit #[code src/Counter.svelte] to trigger HMR
-    Counter(count="{count}")
-</template>
